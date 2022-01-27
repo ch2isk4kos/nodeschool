7ba22e8650846d9a60d641650d27a7fd54444911
ch2isk4kos <ch2isk4kos@gmail.com>
2022-01-26 22:09:47 -0500

    add file for object properties module

--- core/javascripting/object-properties.js
+++ core/javascripting/object-properties.js
@@ -1,4 +1,20 @@
 /*
   You can access and manipulate object properties –– the keys and values
-  that an object contains –– using a method very similar to arrays.
+  that an object contains –– using a method very similar to arrays called bracket notation:
+
+      const example = {
+       pizza: 'yummy'
+     }
+
+     console.log(example['pizza'])
+
+  Alternately, you can use dot notation to get identical results:
+
+     console.log(example.pizza)
 */
+
+const food = {
+  types: "only pizza",
+};
+
+console.log(food.types);
