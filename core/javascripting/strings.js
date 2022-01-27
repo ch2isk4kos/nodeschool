ccdd381a80e710c38b70f4c9433f8c35071afbd5
ch2isk4kos <ch2isk4kos@gmail.com>
2022-01-26 19:13:43 -0500

    add file for strings module

---
+++ core/javascripting/strings.js
@@ -0,0 +1,8 @@
+/**
+  A string is a sequence of characters. A character is, roughly speaking, a
+  written symbol. Examples of characters are letters, numbers, punctuation
+  marks, and spaces.
+*/
+
+const someString = "this is a string";
+console.log(someString);
