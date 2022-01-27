66d181666f2357141685d84b435c99ca9d7a9222
ch2isk4kos <ch2isk4kos@gmail.com>
2022-01-26 21:33:22 -0500

    add access array values module

---
+++ core/javascripting/accessing-array-values.js
@@ -0,0 +1,10 @@
+/*
+  Array elements can be accessed through index number.
+  Index number starts from zero to array's property length minus one.
+  
+  Array elements must be accessed through only using bracket notation.
+  Dot notation is invalid.
+*/
+
+const food = ["apple", "pizza", "pear"];
+console.log(food[1]); // pizza
