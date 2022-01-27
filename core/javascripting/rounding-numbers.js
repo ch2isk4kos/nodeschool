397bc33ef7fed2c9b8e3517cec83910f3878df90
ch2isk4kos <ch2isk4kos@gmail.com>
2022-01-26 20:58:37 -0500

    add file for rounding numbers module

---
+++ core/javascripting/rounding-numbers.js
@@ -0,0 +1,10 @@
+/*
+  We can do basic math using familiar operators like +, -, *, /, and %.
+  For more complex math, we can use the Math object.
+
+  Math.round() is a method that rounds either up or down to the nearest integer.
+*/
+
+let roundUp = 1.5;
+let rounded = Math.round(roundUp);
+console.log(rounded);
