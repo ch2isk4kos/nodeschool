09c84f95636e8bc34e86df6fdbb01ef8bd583b8b
ch2isk4kos <ch2isk4kos@gmail.com>
2022-01-26 21:24:15 -0500

    add for loop  module

---
+++ core/javascripting/if-statement.js
@@ -0,0 +1,9 @@
+/*
+  Conditional statements are used to alter the control flow of a program,
+  based on a specified boolean condition.
+*/
+const fruit = "orange";
+
+if (fruit.length > 5)
+  return console.log("The fruit name has more than five characters.");
+else console.log("The fruit name has five characters or less.");
