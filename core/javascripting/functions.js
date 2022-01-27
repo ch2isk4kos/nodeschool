12f210c99245f17eb6083d080044763d5a6a313b
ch2isk4kos <ch2isk4kos@gmail.com>
2022-01-26 22:15:04 -0500

    add file for functions module

---
+++ core/javascripting/functions.js
@@ -0,0 +1,18 @@
+/*
+  A function is a block of code that takes input, processes that input, and
+  then produces output.
+
+    function example (x) {
+      return x * 2
+    }
+
+  We can call that function like this to get the number 10:
+
+    example(5)
+*/
+
+const eat = (food) => {
+  return food + " tasted really good.";
+};
+
+console.log(eat("bananas"));
