97b3e3a02ad348ccae5af669bf66d1a816501d14
ch2isk4kos <ch2isk4kos@gmail.com>
2022-02-19 14:45:53 -0500

    add solution for concat module

---
+++ core/stream-adventure/concat.js
@@ -0,0 +1,57 @@
+/*
+  You will be given text on process.stdin, convert buffer to string and
+  reverse it using the concat-stream module before writing it to
+  process.stdout.
+
+  concat-stream is a writable stream that concatenate all buffers from a
+  stream and give you the result in the callback you pass like parameter.
+
+  Here's an example that uses concat-stream to buffer POST content in order
+  to JSON.parse() the submitted data:
+
+    const concat = require('concat-stream');
+    const http = require('http');
+
+    const server = http.createServer(function (req, res) {
+      if (req.method === 'POST') {
+        req.pipe(concat(function (body) {
+          const obj = JSON.parse(body);
+          res.end(Object.keys(obj).join('\n'));
+        }));
+      }
+      else res.end();
+    });
+    server.listen(5000);
+
+  In your adventure you'll only need to buffer input with concat() from
+  process.stdin.
+
+  Make sure to npm install concat-stream in the directory where your
+  solution file is located.
+
+  ## Hint:
+
+  Both process.stdout and concat-stream are writeable streams, so they can't
+  be piped together.
+*/
+
+"use strict";
+let buffer = "";
+process.stdin
+  .on("data", function (data) {
+    buffer = buffer + data;
+  })
+  .on("end", function () {
+    // process.stdout.write(buffer.toString().trim().split("").reverse().join(""));
+    process.stdout.write(buffer.toString().split("").reverse().join(""));
+  });
+
+// STREAM-ADVENTURE WITH concat-stream DEPENDENCY SOLUTION:
+// const concat = require("concat-stream");
+
+// process.stdin.pipe(
+//   concat(function (src) {
+//     const s = src.toString().split("").reverse().join("");
+//     process.stdout.write(s);
+//   })
+// );
