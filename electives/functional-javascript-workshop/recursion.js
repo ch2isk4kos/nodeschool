71f48e20a5fd1cdff4b9f2dee076bba22e6c2f77
ch2isk4kos <ch2isk4kos@gmail.com>
2022-02-24 18:45:09 -0500

    add solution for recursion module

--- electives/functional-javascript-workshop/recursion.js
+++ electives/functional-javascript-workshop/recursion.js
@@ -1,88 +1,66 @@
-const reduce = require("./reduce");
-
 /*
-  Recursion is a fundamental programming concept which can lead to elegant and 
-  efficient solutions to algorithmic problems. In fact, recursion is so powerful,
-  all iterating behaviour can be defined using recursive functions.
+  ## Task
+
+  Implement a recursive function that returns all of the unique dependencies,
+  and sub-dependencies of a module, sorted alphabetically.
   
-  You will find recursion indispensable when iterating over nested data structures.
+  Dependencies should be printed as dependency@version e.g. []()'.
 
-  A recursive function is a function which calls itself. For example,
-  this recursive function will take an array of words, and return an array of those words, uppercased.
+  Multiple versions of the same module are allowed,
+  but duplicates modules of the same version should be removed.
 
-    function toUpperArray(items) {
-       if (!items.length) return []             // end condition
-       var head = items[0]                      // item to operate on
-       head = head.toUpperCase()                // perform action
-       var tail = items.slice(1)                // next
-       return [head].concat(toUpperArray(tail)) // recursive step
+  ## Arguments:
+
+  * tree: A dependency tree. See below for an example of the structure.
+
+  ## Example
+
+    var loremIpsum = {
+      "name": "lorem-ipsum",
+      "version": "0.1.1",
+      "dependencies": {
+        "optimist": {
+          "version": "0.3.7",
+          "dependencies": {
+            "wordwrap": {
+              "version": "0.0.2"
+            }
+          }
+        },
+        "inflection": {
+          "version": "1.2.6"
+        }
+      }
     }
 
-    toUpperArray(['hello', 'world']) // => ['HELLO', 'WORLD']
+    getDependencies(loremIpsum) // => [ 'inflection@1.2.6', 'optimist@0.3.7', 'wordwrap@0.0.2' ]
 
-The point of this exercise is to familiarise yourself with recursion by
-implementinga familiar interface using a recursive function.
-
-# Task
-
-Implement Array#reduce using recursion.
-
-To test your reduction works correctly we will use your reduce implementation
-to execute our solution to the previous basic_reduce problem.
-
-i.e. your reduce function will be passed an array of words, and a function,
-     and an initial value which will return an object containing the counts
-     for each word found in the array.
-
-You don't need to implement this functionality, it will be supplied to your reduce implementation.
-
-For simplicity, your implementation of reduce need not replicate the behaviour of a reduce missing an initial value.
-You may assume the initial value will always be supplied.
-
-## Arguments
-
-  * arr: An Array to reduce over
-  * fn: Function to use as the reduction step. Like regular Array#reduce, this function must be passed previousValue, currentValue, index and the array we're iterating over.
-  * init: Initial value of the reduction. Unlike Array#reduce, this value is required (and you may assume it will always be supplied).
-
-## Example
-
-    // Your reduce function should behave the same as a
-    // regular Array#reduce, but it will take the array
-    // to operate on as the first argument:
-
-    reduce([1,2,3], function(prev, curr, index, arr) {
-      return prev + curr
-    }, 0)
-    // => 6
-
-## Conditions
+  ## Conditions:
 
   * Do not use any for/while loops.
-  * Do not use any Array methods like Array#map or Array#reduce.
 
-## Resources
+  ## Boilerplate
 
-  * https://en.wikipedia.org/wiki/Recursion
-  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-
-## Boilerplate
-
-    function reduce(arr, fn, initial) {
+    function getDependencies(tree) {
       // SOLUTION GOES HERE
+      // Note: Feel free to add additional arguments
+      // to this function for use with recursive calls.
+      // Or not! There are many ways to recurse.
     }
 
-    module.exports = reduce
+    module.exports = getDependencies
 */
-module.exports = function reduce(arr, fn, initial) {
-  if (arr.length == 0) return initial;
-  return reduce(arr.slice(1, arr.length), fn, fn(initial, arr[0]));
-};
+module.exports = function getDependencies(pkg, result) {
+  // There are many ways to recurse.
 
-// IIFE SOLUTION
-// module.exports = function reduceIIFE(arr, fn, initial) {
-//   return (function reduceOne(index, value) {
-//     if (index > arr.length - 1) return value; // end condition
-//     return reduceOne(index + 1, fn(value, arr[index], index, arr)); // calculate & pass values to next step
-//   })(0, initial); // IIFE. kick off recursion with initial values
-// };
+  result = result || [];
+  let dependencies = (pkg && pkg.dependencies) || [];
+
+  Object.keys(dependencies).forEach(function (dep) {
+    let key = dep + "@" + pkg.dependencies[dep].version;
+    if (result.indexOf(key) === -1) result.push(key);
+    getDependencies(pkg.dependencies[dep], result);
+  });
+
+  return result.sort();
+};
