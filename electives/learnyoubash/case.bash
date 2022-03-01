903df000c04fae51bd1433ae25637332c1185d7f
ch2isk4kos <ch2isk4kos@gmail.com>
2022-03-01 16:55:04 -0500

    add solution for case module

---
+++ electives/learnyoubash/case.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+case "$1" in
+  (png)
+    echo "It is png."
+    ;;
+  (jpg|jpeg)
+    echo "It is jpeg."
+    ;;
+  (gif)
+    echo "It is gif."
+  ;;
+  (*)
+    echo "$1 is not an image!"
+  ;;
+esac
