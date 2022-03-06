16ada4e843ab688f1a92c9ba5e5bb87a93049f68
ch2isk4kos <ch2isk4kos@gmail.com>
2022-03-06 16:01:09 -0500

    add solution for good old form module

--- electives/expressworks/index.js
+++ electives/expressworks/index.js
@@ -1,12 +1,15 @@
 const path = require("path");
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
 const PORT = process.argv[2] || 3000;
 
 // middleware
-app.use(express.static(process.argv[3] || path.join(__dirname, "public")));
+app.use(express.static(process.argv[3] || path.join(__dirname, "public"))); // static files
+app.use(bodyParser.urlencoded({ extended: false })); // body-parser
 
+// pug templates
 app.set("view engine", "pug");
 app.set("views", process.argv[3] || path.join(__dirname, "templates"));
 
@@ -18,6 +21,11 @@ app.get("/", function (req, res) {
 app.get("/home", function (req, res) {
   // res.end("Hello World!");
   res.render("index", { date: new Date().toDateString() });
+});
+
+app.post("/form", function (req, res) {
+  let payload = req.body.str.split("").reverse().join("");
+  res.send(payload);
 });
 
 // server
