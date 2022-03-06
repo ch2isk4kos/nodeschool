d03c8b92b807ad3fff029cd945d76a5bdd24255c
ch2isk4kos <ch2isk4kos@gmail.com>
2022-03-06 15:45:40 -0500

    add solution for pug module

--- electives/expressworks/index.js
+++ electives/expressworks/index.js
@@ -7,13 +7,17 @@ const PORT = process.argv[2] || 3000;
 // middleware
 app.use(express.static(process.argv[3] || path.join(__dirname, "public")));
 
+app.set("view engine", "pug");
+app.set("views", process.argv[3] || path.join(__dirname, "templates"));
+
 // routes
 app.get("/", function (req, res) {
-  res.end("Home");
+  res.end("Landing Page");
 });
 
 app.get("/home", function (req, res) {
-  res.end("Hello World!");
+  // res.end("Hello World!");
+  res.render("index", { date: new Date().toDateString() });
 });
 
 // server
