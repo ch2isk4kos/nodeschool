ad99bb94760a13a44ac51ca8446bdd80ab9e7334
ch2isk4kos <ch2isk4kos@gmail.com>
2022-03-06 15:11:16 -0500

    add solution for static module

--- electives/expressworks/index.js
+++ electives/expressworks/index.js
@@ -1,8 +1,13 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
 const PORT = process.argv[2] || 3000;
 
+// middleware
+app.use(express.static(process.argv[3] || path.join(__dirname, "public")));
+
+// routes
 app.get("/", function (req, res) {
   res.end("Home");
 });
@@ -11,6 +16,7 @@ app.get("/home", function (req, res) {
   res.end("Hello World!");
 });
 
+// server
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
