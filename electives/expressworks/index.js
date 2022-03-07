d52047b563f4340ffdc587fb6e402ceb3bdb6c7d
ch2isk4kos <ch2isk4kos@gmail.com>
2022-03-06 22:24:58 -0500

    add solution for param pam pam solution

--- electives/expressworks/index.js
+++ electives/expressworks/index.js
@@ -36,6 +36,16 @@ app.post("/form", function (req, res) {
   res.send(payload);
 });
 
+app.put("/message/:id", function (req, res) {
+  let id = req.params.id;
+  let str = require("crypto")
+    .createHash("sha1")
+    .update(new Date().toDateString() + id)
+    .digest("hex");
+
+  res.send(str);
+});
+
 // server
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
